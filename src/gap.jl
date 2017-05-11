5c6a33dae831bd871c4ab3a43f8e262371927ba1
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2017-05-11 13:05:17 +0100

    More API prototyping

--- src/gap.jl
+++ src/gap.jl
@@ -15,3 +15,26 @@ function EvalString(str :: String)
     // return list of obj
     // win.
 end
+
+function to_gap(str :: String)
+    return libgap_StringObj_String(str)
+end
+
+function to_gap(v :: Int32)
+    return libgap_IntObj_Int(v)
+end
+
+function to_gap(v :: Int64)
+    return libgap_IntObj_Int(v)
+end
+
+function to_gap(v :: Array{GapObj, 1})
+end
+
+function GAPFunction(str::name)
+    return function(args...)
+        func = libgap_ValueGlobal(name)
+        gargs = []
+        return libgap_CallFuncList(func, gargs)
+    end
+end
