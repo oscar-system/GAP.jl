c3b65d355af3cdc1db0f42058d20cf5a2ecbbd90
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2018-05-03 12:56:00 +0200

    Various improvements to examples, three new macros for Julia

--- JuliaInterface/example/gap_function_call.jl
+++ JuliaInterface/example/gap_function_call.jl
@@ -1,3 +1,13 @@
 function IsPrimeInt(a)
     return GAP.IsPrimeInt(a)
 end
+
+function Power2(a)
+    # can be done automatic
+    number = Int((Int(a.ptr)-1)/4)
+    number = number ^ 2
+    # is automatic
+    number = number*4 + 1
+    # can be done automatic
+    return GAP.GapObj( Ptr{Void}( number ) )
+end
