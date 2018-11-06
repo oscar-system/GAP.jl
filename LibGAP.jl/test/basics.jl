62d919b929646919ace73baf2e3a98e645ad1bc9
Max Horn <max@quendi.de>
2018-11-06 15:50:14 +0100

    Move GAPFuncs to LibGap.jl and rename it to GAP.Globals
    Resolves #123

--- LibGAP.jl/test/basics.jl
+++ LibGAP.jl/test/basics.jl
@@ -1,5 +1,5 @@
 @testset "basics" begin
-    @test GAP.CSTR_STRING(GAP.GAPFuncs.String(GAP.GAPFuncs.PROD(2^59,2^59))) == "332306998946228968225951765070086144"
+    @test GAP.CSTR_STRING(GAP.Globals.String(GAP.Globals.PROD(2^59,2^59))) == "332306998946228968225951765070086144"
 
     l = GAP.to_gap([1,2,3])
 
