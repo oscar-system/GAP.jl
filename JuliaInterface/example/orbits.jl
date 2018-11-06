9c15c73a11b3517895a52e832daa273b47dd4ffd
Max Horn <max@quendi.de>
2018-11-06 21:58:10 +0100

    Replace GAP orbit implementation
    The new one is much faster. Indeed, for me it is faster than the Julia
    version. However, that also could be optimized; the main reason I did not yet
    do that is that one needs to access a hash function for the orbit elements
    (here: GAP integers), and I did not yet bother to figure out how to do that.

--- JuliaInterface/example/orbits.jl
+++ JuliaInterface/example/orbits.jl
@@ -1,25 +1,15 @@
 function bahn( element, generators, action )
-    work_set = [element]
-    return_set = [element]
-    action_func = GAP.GapFunc( action )
-    generator_length = length(generators)
-    while length(work_set) != 0
-        current_element = pop!(work_set)
-        for current_generator_number = 1:generator_length
-            current_generator = generators[current_generator_number]
-            current_result = action(current_element,current_generator)
-            is_in_set = false
-            for i in return_set
-                if i.ptr == current_result.ptr
-                    is_in_set = true
-                    break
-                end
-            end
-            if ! is_in_set
-                push!( work_set, current_result )
-                push!( return_set, current_result )
+    orb = [ element ]
+    dict = Dict(element=>1)
+    for b in orb
+        for g in generators
+            c = action(b, g)
+            x = get(dict, c, nothing)
+            if x == nothing
+                push!( orb, c )
+                dict[c] = length(orb)
             end
         end
     end
-    return ccall(:NewJuliaObj,Ptr{CVoid},(Ptr{CVoid},),pointer_from_objref(return_set))
+    return orb
 end
