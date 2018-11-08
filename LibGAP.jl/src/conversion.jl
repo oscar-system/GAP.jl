2236eea1560865781ee4fe9d0f9721aef21e16d1
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2018-11-08 16:42:57 +0100

    Use JuliaInterface conversion methods in LibGAP.jl

--- LibGAP.jl/src/conversion.jl
+++ LibGAP.jl/src/conversion.jl
@@ -1,32 +1,30 @@
-## Conversion to GAP
 
-to_gap(v :: Int64)      = v
-to_gap(v :: MPtr)       = v
-to_gap(v :: GapFFE)     = v
-to_gap(v :: String)     = MakeString(v)
-to_gap(v :: Symbol)     = to_gap(string(v))
-to_gap(v :: Bool)       = v
+to_gap(x::Int64) = x
+to_gap(x::GapFFE) = x
+to_gap(x::MPtr) = x
 
-function to_gap(v :: Array{T, 1} where T) :: MPtr
-    l = NewPlist(length(v))
-    for i in 1:length(v)
-        l[i] = to_gap(v[i])
+function to_gap(obj::Array{T,1}) where T
+    len = length(obj)
+    ret_val = NewPlist(len)
+    for i in 1:len
+        ret_val[i] = to_gap(obj[i])
     end
-    return l
+    return ret_val
 end
 
-## Conversion from GAP
-##
-## FIXME: these are not very user friendly (user has to know type of the GAP
-## object in advanced), and some may even crash. It is not even clear that
-## here is the right place to perform most of these conversions, maybe they
-## should be done on the GAP and/or in the JuliaInterface GAP kernel
-## extension instead. We need to work this out...
+from_gap(obj::Int64) = obj
+from_gap(obj::Int64,::Any) = obj
+from_gap(obj::MPtr) = from_gap(obj,Any)
+from_gap(obj::MPtr,::Type{Symbol}) =  Symbol(from_gap(obj))
 
-
-from_gap(obj        , ::Any)                  = obj
-from_gap(obj :: MPtr, ::Type{AbstractString}) = CSTR_STRING(obj)
-from_gap(obj :: MPtr, ::Type{Symbol})         = Symbol(from_gap(obj,AbstractString))
+function from_gap( obj :: MPtr, ::Type{Array{Any,1}} )
+    len_list = length(obj)
+    new_array = Array{Any,1}( undef, len_list)
+    for i in 1:len_list
+        new_array[ i ] = obj[i]
+    end
+    return new_array
+end
 
 function from_gap( obj :: MPtr, ::Type{Array{T,1}} ) where T
     len_list = length(obj)
