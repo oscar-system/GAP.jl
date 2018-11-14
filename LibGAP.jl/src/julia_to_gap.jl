5ad763678e9a7331ce2902cad1546f56cf431553
Max Horn <max@quendi.de>
2018-11-14 17:14:19 +0100

    Rename to_gap -> julia_to_gap

--- LibGAP.jl/src/julia_to_gap.jl
+++ LibGAP.jl/src/julia_to_gap.jl
@@ -1,25 +1,25 @@
 ## Converters
 
-to_gap(x::Int64)  = x
-to_gap(x::Int32)  = Int64(x)
-to_gap(x::Int16)  = Int64(x)
-to_gap(x::Int8)   = Int64(x)
-to_gap(x::UInt64) = BigInt(x)
-to_gap(x::UInt32) = Int64(x)
-to_gap(x::UInt16) = Int64(x)
-to_gap(x::UInt8)  = Int64(x)
+julia_to_gap(x::Int64)  = x
+julia_to_gap(x::Int32)  = Int64(x)
+julia_to_gap(x::Int16)  = Int64(x)
+julia_to_gap(x::Int8)   = Int64(x)
+julia_to_gap(x::UInt64) = BigInt(x)
+julia_to_gap(x::UInt32) = Int64(x)
+julia_to_gap(x::UInt16) = Int64(x)
+julia_to_gap(x::UInt8)  = Int64(x)
 
 
 
 
-to_gap(x::GapFFE) = x
-to_gap(x::MPtr) = x
+julia_to_gap(x::GapFFE) = x
+julia_to_gap(x::MPtr) = x
 
-function to_gap(obj::Array{T,1}) where T
+function julia_to_gap(obj::Array{T,1}) where T
     len = length(obj)
     ret_val = NewPlist(len)
     for i in 1:len
-        ret_val[i] = to_gap(obj[i])
+        ret_val[i] = julia_to_gap(obj[i])
     end
     return ret_val
 end
