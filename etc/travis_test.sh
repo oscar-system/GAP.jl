70e916b7aac03114c246386089a5a75f1471229a
Max Horn <max@quendi.de>
2018-11-20 11:54:46 +0100

    Travis: signal test failure only after all three test suites run

--- etc/travis_test.sh
+++ etc/travis_test.sh
@@ -5,6 +5,8 @@ set -e
 export CFLAGS=--coverage
 export LDFLAGS=--coverage
 
+AnyFailures=No
+
 # check code formatting
 find . -name '*.c' -exec clang-format -i {} \;
 find . -name '*.h' -exec clang-format -i {} \;
@@ -17,19 +19,24 @@ make
 #
 cd JuliaInterface
 pwd
-$GAPROOT/gap -A --quitonbreak --norepl --cover $TRAVIS_BUILD_DIR/coverage/JuliaInterface tst/testall.g
+$GAPROOT/gap -A --quitonbreak --norepl --cover $TRAVIS_BUILD_DIR/coverage/JuliaInterface tst/testall.g || AnyFailures=Yes
 gcov -o .. src/*.c*
 cd ..
 
 #
 cd JuliaExperimental
 pwd
-$GAPROOT/gap -A --quitonbreak --norepl --cover $TRAVIS_BUILD_DIR/coverage/JuliaExperimental tst/testall.g
+$GAPROOT/gap -A --quitonbreak --norepl --cover $TRAVIS_BUILD_DIR/coverage/JuliaExperimental tst/testall.g || AnyFailures=Yes
 gcov -o .. src/*.c*
 cd ..
 
 #
 cd LibGAP.jl
 pwd
-${JULIAROOT}/bin/julia --code-coverage --inline=no test/runtests.jl
+${JULIAROOT}/bin/julia --code-coverage --inline=no test/runtests.jl || AnyFailures=Yes
 cd ..
+
+if [ ${AnyFailures} = Yes ]
+then
+    exit 1
+fi
