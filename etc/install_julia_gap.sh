b01b750aec6331c39c0e665501f9dcee9a87416b
Frank Lübeck <frank.luebeck@math.rwth-aachen.de>
2019-01-25 17:54:28 +0100

    Installation of all OSCAR parts for GAPJulia
    This installs and fully builds on my Debian 9 installation
    inside the current directory
       gap (master)
          with pkgfull
          and GAPJulia
       julia (latest)
          with AbstractAlgebra, Singular, GB, Nemo, Hecke, Polymake
       polymake
    
    Maybe a check could be added to test for the needed Debian/Ubuntu/Mint
    packages?

--- etc/install_julia_gap.sh
+++ etc/install_julia_gap.sh
@@ -9,19 +9,53 @@ wget https://julialangnightlies-s3.julialang.org/bin/linux/x64/julia-latest-linu
 mkdir julia 
 tar xf julia-latest-linux64.tar.gz -C julia --strip-components 1
 
+julia/bin/julia <<UNTILEND
+using Pkg
+Pkg.add("AbstractAlgebra")
+Pkg.add("Nemo")
+#Pkg.develop("Hecke")
+Pkg.add(PackageSpec(url="https://github.com/thofma/Hecke.jl"))
+Pkg.add(PackageSpec(url="https://github.com/oscar-system/Singular.jl"))
+UNTILEND
+
 # Download and install GAP
 git clone --depth=1 https://github.com/gap-system/gap
 cd gap
 ./autogen.sh
 ./configure --with-gc=julia --with-julia=${main_dir}/julia/
 make -j4
-make libgap.la
-make bootstrap-pkg-minimal
+#make libgap.la
+make bootstrap-pkg-full
+cd pkg
+${main_dir}/gap/bin/BuildPackages.sh
+
+# Compile main GAP manuals
+cd $main_dir/gap
+make doc
 
 # Download and install GAPJulia
 if [[ $DONT_FETCH_GAP_JULIA_PKG = yes ]] ; then exit 0; fi
-cd pkg
 git clone https://github.com/oscar-system/GAPJulia
-cd GAPJulia/JuliaInterface
+cd GAPJulia
 ./configure
 make
+
+# Download and install Polymake
+cd $main_dir
+git clone https://github.com/polymake/polymake.git
+mv polymake polymakegit
+cd polymakegit
+git checkout Snapshots
+./configure --prefix=${main_dir}/polymake
+ninja -C build/Opt
+ninja -C build/Opt install
+
+# now Polymake.jl
+cd ${main_dir}
+export POLYMAKE_CONFIG=${main_dir}/polymake/bin/polymake-config
+julia/bin/julia <<UNTILEND
+using Pkg
+Pkg.add(PackageSpec(url="https://github.com/oscar-system/Polymake.jl"))
+UNTILEND
+
+
