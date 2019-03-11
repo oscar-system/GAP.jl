7602b8229fc8d25771a80d453ebdbfd990bb5211
Max Horn <max@quendi.de>
2019-03-11 15:21:07 +0100

    Attempt to fix coverage for GAP kernel extensions

--- etc/travis_test.sh
+++ etc/travis_test.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-export CFLAGS=--coverage
-export LDFLAGS=--coverage
-
 AnyFailures=No
 
 # check code formatting
@@ -13,19 +10,18 @@ find . -name '*.h' -exec clang-format -i {} \;
 git diff --exit-code -- . # detect if there are any diffs
 
 
-
 #
 cd ${TRAVIS_BUILD_DIR}/pkg/GAPJulia/JuliaInterface
 pwd
-${HOME}/.julia/gap -A --quitonbreak --norepl --cover ${TRAVIS_BUILD_DIR}/coverage/JuliaInterface tst/testall.g || AnyFailures=Yes
-gcov -o .. src/*.c*
+${HOME}/.julia/gap -A --quitonbreak --norepl --cover ${TRAVIS_BUILD_DIR}/coverage/JuliaInterface.coverage tst/testall.g || AnyFailures=Yes
+gcov -o $HOME/.julia/packages/GAP/*/pkg/GAPJulia/JuliaInterface/gen/src/.libs src/*.c*
 cd ..
 
 #
 cd ${TRAVIS_BUILD_DIR}/pkg/GAPJulia/JuliaExperimental
 pwd
-${HOME}/.julia/gap -A --quitonbreak --norepl --cover ${TRAVIS_BUILD_DIR}/coverage/JuliaExperimental tst/testall.g || AnyFailures=Yes
-gcov -o .. src/*.c*
+${HOME}/.julia/gap -A --quitonbreak --norepl --cover ${TRAVIS_BUILD_DIR}/coverage/JuliaExperimental.coverage tst/testall.g || AnyFailures=Yes
+gcov -o $HOME/.julia/packages/GAP/*/pkg/GAPJulia/JuliaExperimental/gen/src/.libs src/*.c*
 cd ..
 
 #
