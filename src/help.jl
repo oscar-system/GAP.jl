2a64fa0722930f234cbde27c7c6fcdff11db601a
Sebastian Gutsche <gutsche@mathematik.uni-siegen.de>
2019-06-17 17:19:23 +0200

    Enable ? help for `GAP.Globals.FOO`

---
+++ src/help.jl
@@ -0,0 +1,25 @@
+function GAP_help_string(topic::String, onlyexact::Bool = false)
+    return GAP.gap_to_julia(GAP.Globals.HelpString(GAP.julia_to_gap(topic), onlyexact))
+end
+
+function show_GAP_help(topic::String, onlyexact::Bool = false)
+    print(GAP_help_string(topic, onlyexact))
+end
+
+import Base.Docs: Binding, getdoc, docstr
+
+## Create a helper type that gets returned by Binding
+struct GAPHelpType
+    name::Symbol
+end
+
+Base.Docs.Binding(x::GlobalsType, name::Symbol) = GAPHelpType(name)
+
+function Base.Docs.doc(x::GAPHelpType, typesig::Type = Union{})
+    return Text(GAP_help_string(string(x.name)))
+end
+
+## Set getdoc for GlobalsType to nothing,
+## so it dispatches on the Binding.
+Base.Docs.getdoc(x::GlobalsType) = nothing
+Base.Docs.getdoc(x::GapObj) = Text(GAP_help_string(gap_to_julia(Globals.NameFunction(x))))
