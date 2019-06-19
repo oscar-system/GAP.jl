0d8f614ba28b5621d84f0584d58d023f5dce7f6d
Max Horn <max@quendi.de>
2019-06-19 10:08:14 +0200

    Revert "Travis: tweak how coverage is collected"
    This reverts commit 6ce3deb062638b7f9d3fa8f1472b66fee76d3fc2.

--- etc/gather_coverage.jl
+++ etc/gather_coverage.jl
@@ -6,9 +6,8 @@ using Coverage
 import JSON
 
 # gather coverage
-data = process_folder("src")
-data = append!(data, process_folder("pkg"))
-data_dict = to_json(data)
+data = Codecov.process_folder(pwd())
+data_dict = Codecov.to_json(data)
 
 # write it to a file for the upload script
 println("Writing to julia-coverage.json ...")
