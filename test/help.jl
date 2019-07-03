ffe09f8b5d5c28340670ebb81ff0ac14550e2937
Max Horn <max@quendi.de>
2019-07-03 22:41:06 +0200

    test/help.jl: Lookup something in the JuliaInterface manual

--- test/help.jl
+++ test/help.jl
@@ -15,6 +15,8 @@
     @test test_gap_help( "welcome to gap" )
 
     @test test_gap_help( "?determinant" )
+    @test test_gap_help( "?IsJuliaWrapper" )
+    println(GAP.GAP_help_string( "?IsJuliaWrapper" ))
 
     @test test_gap_help( "books" )
     @test test_gap_help( "tut:chapters" )
