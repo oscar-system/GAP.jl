38e68489658f5158f4cd6a9b2b812400a87dbb6d
Max Horn <max@quendi.de>
2020-01-23 17:46:41 +0100

    Fix tests
    ... and this is why you should always use PRs, even for trivial changes
    (like this one... *cough*)

--- test/conversion.jl
+++ test/conversion.jl
@@ -120,13 +120,13 @@
 
     ## Catch conversions to types that are not supported
     xx = GAP.julia_to_gap( "a" )
-    @test_throws MethodError GAP.gap_to_julia( Dict{Int64,Int64}, xx )
+    @test_throws ErrorException GAP.gap_to_julia( Dict{Int64,Int64}, xx )
 
     ## Test converting GAP lists with holes in them
     xx = GAP.EvalString( "[1,,1]" )
     @test GAP.gap_to_julia(xx) == Any[1,nothing,1]
     @test GAP.gap_to_julia(Array{Any,1},xx) == Any[1,nothing,1]
-    @test_throws MethodError GAP.gap_to_julia(Array{Int64,1},xx)
+    @test_throws ErrorException GAP.gap_to_julia(Array{Int64,1},xx)
     @test GAP.gap_to_julia(Array{Union{Nothing,Int64},1},xx) == Union{Nothing,Int64}[1,nothing,1]
     @test GAP.gap_to_julia(Array{Union{Int64,Nothing},1},xx) == Union{Nothing,Int64}[1,nothing,1]
 
