e4dd65b50f9721caef9faaf35d4f9c948f838559
kalmarek <kalmar@amu.edu.pl>
2020-04-01 13:44:30 +0200

    add hash of source tarball to build.jl
    don't download a new one if the old is present

--- deps/build.jl
+++ deps/build.jl
@@ -2,8 +2,12 @@
 # and exposes the right paths to julia/GAP
 
 import Pkg
+import SHA
 
 gap_version = v"4.11.0"
+sources_sha512 = Dict(
+    "gap-4.11.0.tar.bz2" => "1627a19dc9a156752773d24cd624ee1ebbf5c94180f7cc59502022597d3eac2fff4bbf6d5dc57b1a5c1e4d94e93105e354ba46ef959dfdfd699c87d8d2809761",
+    )
 
 extra_gap_root = abspath(joinpath(@__DIR__, ".."))
 gap_root = abspath(joinpath(extra_gap_root, "gap-$(gap_version)"))
@@ -18,6 +22,13 @@ println("gap_root = ", gap_root)
 println("extra_gap_root = ", extra_gap_root)
 println("install_gap = ", install_gap)
 
+function check_hash(fname::AbstractString, hashfunc, hash_value::String)
+    h = open(fname, "r") do file
+        hashfunc(file)
+    end
+    return bytes2hex(h) == hash_value
+end
+
 ## Find julia binary
 julia_binary = get(ENV, "JULIA_BINARY", Sys.BINDIR)
 
@@ -25,12 +36,18 @@ julia_binary = get(ENV, "JULIA_BINARY", Sys.BINDIR)
 if install_gap
     println("Installing GAP ...")
     cd(extra_gap_root)
-    run(`rm -rf gap-$(gap_version)`)
-    # TODO: gap-system.org availability is flaky, which often breaks CI builds; so use
-    # github mirror instead, which is more reliable, albeit somewhat slower
-    #filename = download("https://www.gap-system.org/pub/gap/gap-4.$(gap_version.minor)/tar.bz2/gap-$(gap_version).tar.bz2")
-    filename = download("https://github.com/gap-system/gap/releases/download/v$(gap_version)/gap-$(gap_version).tar.bz2")
-    run(`tar xjf $(filename)`)
+    gap_build_packages =  get(ENV, "GAP_BUILD_PACKAGES", "no")
+    gap_source_tarball = "gap-$(gap_version).tar.bz2"
+    if !isfile(gap_source_tarball) || !check_hash(gap_source_tarball, SHA.sha512, sources_sha512[gap_source_tarball])
+        println("Sources for GAP $gap_version don't exist or don't match the sha sum; downloading...")
+        rm(gap_source_tarball, force=true)
+        # TODO: gap-system.org availability is flaky, which often breaks CI builds; so use
+        # github mirror instead, which is more reliable, albeit somewhat slower
+        # download("https://www.gap-system.org/pub/gap/gap-4.$(gap_version.minor)/tar.bz2/$(gap_source_tarball)", gap_source_tarball)
+        download("https://github.com/gap-system/gap/releases/download/v$(gap_version)/$(gap_source_tarball)", gap_source_tarball)
+    end
+    @assert check_hash(gap_source_tarball, SHA.sha512, sources_sha512[gap_source_tarball])
+    run(`tar xjf $(gap_source_tarball)`)
     cd("gap-$(gap_version)")
     run(`./configure --with-gc=julia --with-julia=$(julia_binary)`)
     run(`make -j$(Sys.CPU_THREADS)`)
