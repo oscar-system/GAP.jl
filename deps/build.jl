4368a51203218ba09d89e45e9e7b0bd6c2f234fc
Max Horn <max@quendi.de>
2020-04-04 00:27:32 +0200

    Remove special support for using a self-compiled GAP
    This is not something we want to encourage in general, and it's only
    really useful for GAP.jl developers; and even that only rarely: when
    testing against GAP master, for example, which we might want to do again
    in the future. But then we can use artifact overrides, or simply copy /
    symlink the required files into the right place.

--- deps/build.jl
+++ deps/build.jl
@@ -7,48 +7,39 @@ gap_version = v"4.11.0"
 
 extra_gap_root = abspath(joinpath(@__DIR__, ".."))
 gap_root = abspath(joinpath(extra_gap_root, "gap-$(gap_version)"))
-install_gap = true
-
-if haskey(ENV, "GAPROOT")
-    gap_root = ENV["GAPROOT"]
-    install_gap = false
-end
 
 println("gap_root = ", gap_root)
 println("extra_gap_root = ", extra_gap_root)
-println("install_gap = ", install_gap)
 
 ## Find julia binary
 julia_binary = get(ENV, "JULIA_BINARY", Sys.BINDIR)
 
 ## Install GAP
-if install_gap
-    println("Installing GAP ...")
-    cd(extra_gap_root)
-    run(`rm -rf gap-$(gap_version)`)
-    # TODO: gap-system.org availability is flaky, which often breaks CI builds; so use
-    # github mirror instead, which is more reliable, albeit somewhat slower
-    #filename = download("https://www.gap-system.org/pub/gap/gap-4.$(gap_version.minor)/tar.bz2/gap-$(gap_version).tar.bz2")
-    filename = download("https://github.com/gap-system/gap/releases/download/v$(gap_version)/gap-$(gap_version).tar.bz2")
-    run(`tar xjf $(filename)`)
-    cd("gap-$(gap_version)")
-    run(`./configure --with-gc=julia --with-julia=$(julia_binary)`)
-    run(`make -j$(Sys.CPU_THREADS)`)
+println("Installing GAP ...")
+cd(extra_gap_root)
+run(`rm -rf gap-$(gap_version)`)
+# TODO: gap-system.org availability is flaky, which often breaks CI builds; so use
+# github mirror instead, which is more reliable, albeit somewhat slower
+#filename = download("https://www.gap-system.org/pub/gap/gap-4.$(gap_version.minor)/tar.bz2/gap-$(gap_version).tar.bz2")
+filename = download("https://github.com/gap-system/gap/releases/download/v$(gap_version)/gap-$(gap_version).tar.bz2")
+run(`tar xjf $(filename)`)
+cd("gap-$(gap_version)")
+run(`./configure --with-gc=julia --with-julia=$(julia_binary)`)
+run(`make -j$(Sys.CPU_THREADS)`)
 
-    gap_build_packages =  get(ENV, "GAP_BUILD_PACKAGES", "no")
-    if gap_build_packages == "yes"
-        cd("pkg")
-        # eliminate a few big packages that take long to compile
-        pkgs = Base.Filesystem.readdir()
-        pkgs = Base.filter(x -> occursin(r"^(Normaliz|semigroups|simpcomp)", x), pkgs)
-        run(`rm -rf $pkgs`)
-        run(`../bin/BuildPackages.sh`)
-    elseif gap_build_packages == "debug"
-        cd("pkg")
-        pkgs = Base.Filesystem.readdir()
-        pkgs = Base.filter(x -> occursin(r"^(io|profiling)", x), pkgs)
-        run(`../bin/BuildPackages.sh $pkgs`)
-    end
+gap_build_packages =  get(ENV, "GAP_BUILD_PACKAGES", "no")
+if gap_build_packages == "yes"
+    cd("pkg")
+    # eliminate a few big packages that take long to compile
+    pkgs = Base.Filesystem.readdir()
+    pkgs = Base.filter(x -> occursin(r"^(Normaliz|semigroups|simpcomp)", x), pkgs)
+    run(`rm -rf $pkgs`)
+    run(`../bin/BuildPackages.sh`)
+elseif gap_build_packages == "debug"
+    cd("pkg")
+    pkgs = Base.Filesystem.readdir()
+    pkgs = Base.filter(x -> occursin(r"^(io|profiling)", x), pkgs)
+    run(`../bin/BuildPackages.sh $pkgs`)
 end
 
 ##
