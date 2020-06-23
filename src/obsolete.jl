bc8e756daa2c182929e27abddb29224357e00713
ThomasBreuer <sam@math.rwth-aachen.de>
2020-06-23 22:27:04 +0200

    julia_to_gap: handle recursion via keyword argument
    Changed the syntax of `julia_to_gap`:
    Use the keyword argument `recursive` (value `true` or `false`)
    instead of the argument `Val(true)` or `Val(false)`;
    this syntax is used already for `gap_to_julia`.
    
    Adjusted the code that used the old syntax (some tests and
    some conversion functions in JuliaInterface).
    
    The old syntax is still supported, via a method in `src/obsolete.jl`.

--- src/obsolete.jl
+++ src/obsolete.jl
@@ -4,3 +4,10 @@ function EvalString(cmd::String)
     @warn "Use GAP.evalstr instead of GAP.EvalString"
     return evalstr(cmd)
 end
+
+## Deprecated signatures
+
+function julia_to_gap(obj::Any, recursive::Val{Recursive},
+    recursion_dict = IdDict()) where {Recursive}
+    return julia_to_gap(obj, recursion_dict; recursive = Recursive)
+end
