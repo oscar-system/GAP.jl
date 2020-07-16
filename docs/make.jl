37033a28ce88a86bca88c0ac4f5ffb5e3d041725
ThomasBreuer <sam@math.rwth-aachen.de>
2020-07-16 22:05:29 +0200

    document the computation of external references

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,37 @@
 using Documenter, GAP, Markdown
 
+## The following code inserts a step into the list of tasks
+## that are performed by Documenter.jl's function `makedocs`.
+## The idea is to encode external references to GAP manuals
+## by strings of the form `"GAP_ref(<bookname>:<label>)"`
+## in the source files, and to compute the corresponding URLs
+## in the manuals available at `https://www.gap-system.org/Manuals/`
+## when `makedocs` creates GAP.jl's documentation.
+## (Note that these URLs may change when new GAP versions get released,
+## thus one does not want to update them by hand.)
+##
+## The steps for `makedocs` are given by the subtypes of
+## `Documenter.Builder.DocumentPipeline`,
+## they get executed in the order defined by the values (floats)
+## of the function `Documenter.Selectors.order` for these subtypes,
+## and executing the step for the subtype `T` means to call
+## `Documenter.Selectors.runner` with `T` and the document object.
+##
+## The code was adapted from the package
+## https://github.com/ali-ramadhan/DocumenterBibliographyTest.jl,
+## the hint to use this approach came up in the discussion of
+## https://github.com/JuliaDocs/Documenter.jl/issues/1343.
+
+## Declare a new subtype for our step,
+## the evaluation of external references.
 abstract type ExternalReference <: Documenter.Builder.DocumentPipeline end
 
-Documenter.Selectors.order(::Type{ExternalReference}) = 3.1  # After cross-references
+## Execute our step after the computation of cross-references (3.0)
+## and before the check of the document (4.0).
+Documenter.Selectors.order(::Type{ExternalReference}) = 3.1
 
+## When executing our step, print an info line
+## and call the function that does the work.
 function Documenter.Selectors.runner(::Type{ExternalReference}, doc::Documenter.Documents.Document)
     @info "ExternalReference: building external references."
     compute_external_references(doc)
@@ -25,6 +53,7 @@ function compute_external_reference(elem, page, doc)
 end
 
 function compute_external_reference(link, meta, page, doc)
+    # Do something only if the current element has the type `Markdown.Link`.
     if isa(link, Markdown.Link) && startswith(link.url, "GAP_ref")
         @info "Computing external reference: $(link.url)."
         reference = match(r"GAP_ref\((.*)\)", link.url)
@@ -33,10 +62,13 @@ function compute_external_reference(link, meta, page, doc)
             key = split(reference, ":")
             if length(key) == 2
                 key = GAP.julia_to_gap(string(key[2]))
+                # Find all matches of `"<bookname>:<label>"`.
                 urls = GAP.Globals.MatchURLs(GAP.julia_to_gap(reference),
                            GAP.julia_to_gap("https://www.gap-system.org/Manuals/"))
+                # Take only exact matches, this should be unique.
                 urls = GAP.Globals.Filtered(urls, x -> x[2] == key)
                 if length(urls) == 1
+                    # Replace the URL in the link.
                     link.url = String(urls[1][3])
                 end
             end
