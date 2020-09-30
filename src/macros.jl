fcbf107e3334d14a17848e3d2f4c622bc637a5bc
ThomasBreuer <sam@math.rwth-aachen.de>
2020-09-30 17:48:13 +0200

    fixed the @g_str macro
    - changed the code such that escaped characters are handled correctly,
    - added comments/examples concerning the limitations,
    - added a test

--- src/macros.jl
+++ src/macros.jl
@@ -74,10 +74,42 @@ Create a GAP string by typing `g"content"`.
 ```jldoctest
 julia> g"foo"
 GAP: "foo"
+
+julia> g"ab\\ncd\\\"ef\\\\gh"   # special characters are handled as in GAP
+GAP: "ab\\ncd\\\"ef\\\\gh"
+
+```
+
+Due to Julia's way of handing over arguments into the code of macros,
+not all strings representing valid GAP strings can be processed.
+
+```jldoctest
+julia> g"\\\\"
+ERROR: LoadError: failed to convert to GapObj:
+ \\
+[...]
+
+```
+
+Conversely,
+there are valid arguments for the macro that are not valid Julia strings.
+
+```jldoctest
+julia> g"\\c"
+GAP: "\\c"
+
 ```
 """
 macro g_str(str)
-    return julia_to_gap(str)
+    # Note that here backslashes inside `str` are literally contained in `str`,
+    # except for backslashes that escape doublequotes.
+    # In order to get the intended meaning (as stated in the GAP manual section
+    # "Special Characters"),
+    # we escape doublequotes and leave the interpretation to `evalstr`.
+    evl = evalstr("\"" * replace(str, "\"" => "\\\"") * "\"")
+    evl === nothing && error("failed to convert to GapObj:\n $str")
+
+    return evl
 end
 
 export @g_str
