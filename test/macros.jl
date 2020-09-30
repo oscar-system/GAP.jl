12959667e4950a8dd6adbc6bf519b8a743fe1a29
ThomasBreuer <sam@math.rwth-aachen.de>
2020-09-30 17:48:13 +0200

    fixed the @g_str macro
    - changed the code such that escaped characters are handled correctly,
    - added comments/examples concerning the limitations,
    - added a test

--- test/macros.jl
+++ test/macros.jl
@@ -10,4 +10,9 @@
     @test GAP.Globals.Size(x) == 6
     x = GAP.g"foo"
     @test x == GAP.julia_to_gap("foo")
+    x = GAP.g"1:\n, 2:\", 3:\\, 4:\b, 5:\r, 6:\c, 7:\001"
+    @test x == GAP.julia_to_gap("1:\n, 2:\", 3:\\, 4:\b, 5:\r, 6:\003, 7:\001")
+#TODO: Can the following error not be tested?
+#    @test_throws LoadError GAP.g"\\"
+
 end
