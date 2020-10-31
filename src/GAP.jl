993a937942d3389bcd7be28067a87fe4cfd7a70c
Max Horn <max@quendi.de>
2020-10-31 23:39:25 +0100

    Move some code from GAP.jl to separate files

--- src/GAP.jl
+++ src/GAP.jl
@@ -21,98 +21,12 @@ end
 include(deps_jl)
 
 
-"""
-    FFE
-
-Wrap a pointer to a GAP FFE ("finite field element") immediate object.
-This type is defined in the JuliaInterface C code.
-
-# Examples
-```jldoctest
-julia> x = GAP.evalstr( "Z(3)" )
-GAP: Z(3)
-
-julia> typeof( x )
-FFE
-
-```
-"""
-primitive type FFE 64 end
-
-export FFE
-
 import Base: length, finalize
-
 import Libdl
 import Markdown
 import Random
 
-"""
-    GapObj
-
-This is the Julia type of all those GAP objects that are not
-"immediate" (Booleans, small integers, FFEs).
-
-# Examples
-```jldoctest
-julia> isa( GAP.evalstr( "[ 1, 2 ]" ), GapObj ) # a GAP list
-true
-
-julia> isa( GAP.evalstr( "rec()" ), GapObj )    # a GAP record
-true
-
-julia> isa( GAP.evalstr( "(1,2,3)" ), GapObj )  # a GAP permutation
-true
-
-julia> isa( GAP.evalstr( "2^64" ), GapObj )     # a large GAP integer
-true
-
-julia> typeof( GAP.evalstr( "2^59" ) )          # a small GAP integer
-Int64
-
-julia> typeof( GAP.evalstr( "Z(2)" ) )          # a GAP FFE
-FFE
-
-julia> typeof( GAP.evalstr( "true" ) )          # a Boolean
-Bool
-
-```
-
-Note that this is Julia's viewpoint on GAP objects.
-From the viewpoint of GAP, also the pointers to Julia objects are
-implemented as "non-immediate GAP objects",
-but they appear as Julia objects to Julia, not "doubly wrapped".
-
-# Examples
-```jldoctest
-julia> GAP.evalstr( "Julia.Base" )
-Base
-
-julia> typeof( GAP.evalstr( "Julia.Base" ) )        # native Julia object
-Module
-
-```
-
-One can use `GapObj` as a constructor,
-in order to convert Julia objects to GAP objects.
-Such calls are delegated to [`julia_to_gap`](@ref).
-
-# Examples
-```jldoctest
-julia> GapObj(1//3)
-GAP: 1/3
-
-julia> GapObj([1 2; 3 4])
-GAP: [ [ 1, 2 ], [ 3, 4 ] ]
-
-```
-"""
-abstract type GapObj end
-
-export GapObj
-
-# TODO: should we document Obj? What about ForeignGAP.MPtr?
-const Obj = Union{GapObj,FFE,Int64,Bool,Nothing}
+include("types.jl")
 
 function read_sysinfo_gap(dir::String)
     d = missing
@@ -371,55 +285,6 @@ function __init__()
     end
 end
 
-function gap_exe()
-    return joinpath(GAPROOT, "bin", "gap.sh")
-end
-export gap_exe
-
-"""
-    prompt()
-
-Start a GAP prompt where you can enter GAP commands as in a regular GAP
-session. This prompt can be left as any GAP prompt by either entering `quit;`
-or pressing ctrl-D, which returns to the Julia prompt.
-
-This GAP prompt allows to quickly switch between writing Julia and GAP code in
-a session where all data is shared.
-"""
-function prompt()
-    global disable_error_handler
-
-    # save the current SIGINT handler
-    # HACK: the hardcoded value for SIG_DFL is not portable, revise this
-    # install GAP's SIGINT handler
-    old_sigint = ccall(:signal, Ptr{Cvoid}, (Cint, Ptr{Cvoid}), Base.SIGINT, C_NULL)
-
-    # install GAP's SIGINT handler
-    ccall(:SyInstallAnswerIntr, Cvoid, ())
-
-    # restore GAP's error output
-    disable_error_handler = true
-    Globals.MakeReadWriteGlobal(julia_to_gap("ERROR_OUTPUT"))
-    evalstr("""ERROR_OUTPUT:= "*errout*";""")
-    Globals.MakeReadOnlyGlobal(julia_to_gap("ERROR_OUTPUT"))
-
-    # enable break loop
-    Globals.BreakOnError = true
-
-    # start GAP repl
-    Globals.SESSION()
-
-    # disable break loop
-    Globals.BreakOnError = false
-
-    # restore signal handler
-    ccall(:signal, Ptr{Cvoid}, (Cint, Ptr{Cvoid}), Base.SIGINT, old_sigint)
-
-    # restore GAP error handler
-    disable_error_handler = false
-    reset_GAP_ERROR_OUTPUT()
-end
-
 """
     GAP.randseed!([seed::Integer])
 
@@ -451,6 +316,7 @@ include("julia_to_gap.jl")
 include("utils.jl")
 include("help.jl")
 include("packages.jl")
+include("prompt.jl")
 include("obsolete.jl")
 
 end
