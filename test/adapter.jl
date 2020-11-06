b85ac421120be017b70ceae6c824d6a04c070a2b
Rafael Fourquet <fourquet.rafael@gmail.com>
2020-11-06 14:20:15 +0100

    support iteration for non-IsList

--- test/adapter.jl
+++ test/adapter.jl
@@ -6,4 +6,11 @@
     lj = collect(Int, l)
     @test lj isa Vector{Int}
     @test lj == [1, 2, 3]
+
+    s = GAP.Globals.SymmetricGroup(3)
+    xs = []
+    for x in s
+        push!(xs, x)
+    end
+    @test length(xs) == 6
 end
