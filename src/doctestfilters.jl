24df1f8f79959b945eff7cd60c02831837764efc
ThomasBreuer <sam@math.rwth-aachen.de>
2021-01-08 14:11:45 +0100

    added a description how doctestfilters work
    This should better have been part of PR #604, but this is already merged.

--- src/doctestfilters.jl
+++ src/doctestfilters.jl
@@ -1,11 +1,18 @@
 # Define some replacements which are applied in `docs/make.jl` and `test/testmanual.jl`.
+# The idea is that the `jldoctest`ed examples in the source files
+# have the format that fits to the "current" Julia,
+# and that other Julia versions (such as Julia nightly) may show different formats.
+# Each regular expression in `GAP_doctestfilters` gets applied to both the
+# expected output and the output obtained in the actual evaluation,
+# before the comparison of the two values, and matching text gets replaced
+# by the empty string.
 GAP_doctestfilters = Regex[
     r"BitVector|BitArray\{1\}",
-    r"Dict\{Symbol,Any\}|Dict\{Symbol, Any\}",
-    r"Dict\{Symbol,Array\{Int64,1\}\}|Dict\{Symbol, Vector\{Int64\}\}",
-    r"Dict\{Symbol,Int64\}|Dict\{Symbol, Int64\}",
+    r"Dict\{Symbol, Any\}|Dict\{Symbol,Any\}",
+    r"Dict\{Symbol, Vector\{Int64\}\}|Dict\{Symbol,Array\{Int64,1\}\}",
+    r"Dict\{Symbol, Int64\}|Dict\{Symbol,Int64\}",
     r"Matrix\{Int64\}|Array\{Int64,2\}",
-    r"StepRange\{Int8,Int8\}|StepRange\{Int8, Int8\}",
+    r"StepRange\{Int8, Int8\}|StepRange\{Int8,Int8\}",
     r"Vector\{Any\}|Array\{Any,1\}",
     r"Vector\{Int64\}|Array\{Int64,1\}",
     r"Vector\{UInt8\}|Array\{UInt8,1\}",
