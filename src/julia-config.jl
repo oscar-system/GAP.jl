398b6f3ae3f4104129140ff59ab21c330c5d0057
Max Horn <max@quendi.de>
2021-02-13 02:03:36 +0100

    Ship our own copy of julia-config.jl
    Unfortunately, several Linux distributions do not ship julia-config.jl
    in their default Julia package (e.g. Debian, Ubuntu, Manjaro). While
    for Debian and Ubuntu, this can be resolved by installing `libjulia-dev`,
    for Manjaro we found no way at all to get it. Regardless, in either case
    this is an annoying obstacle for people.
    
    I checked, and julia-config.jl has been unchanged since at least Julia 1.3;
    so it should be reasonably safe to include our own copy.

---
+++ src/julia-config.jl
@@ -0,0 +1,67 @@
+# This file is based on the file `contrib/julia-config.jl` which is a
+# part of Julia. License is MIT: https://julialang.org/license
+
+import Libdl
+
+function shell_escape(str)
+    str = replace(str, "'" => "'\''")
+    return "'$str'"
+end
+
+function libDir()
+    return if ccall(:jl_is_debugbuild, Cint, ()) != 0
+        if Base.DARWIN_FRAMEWORK
+            joinpath(dirname(abspath(Libdl.dlpath(Base.DARWIN_FRAMEWORK_NAME * "_debug"))),"lib")
+        else
+            dirname(abspath(Libdl.dlpath("libjulia-debug")))
+        end
+    else
+        if Base.DARWIN_FRAMEWORK
+            joinpath(dirname(abspath(Libdl.dlpath(Base.DARWIN_FRAMEWORK_NAME))),"lib")
+        else
+            dirname(abspath(Libdl.dlpath("libjulia")))
+        end
+    end
+end
+
+private_libDir() = abspath(Sys.BINDIR, Base.PRIVATE_LIBDIR)
+
+function includeDir()
+    return abspath(Sys.BINDIR, Base.INCLUDEDIR, "julia")
+end
+
+function ldflags()
+    fl = "-L$(shell_escape(libDir()))"
+    if Sys.iswindows()
+        fl = fl * " -Wl,--stack,8388608"
+    elseif !Sys.isapple()
+        fl = fl * " -Wl,--export-dynamic"
+    end
+    return fl
+end
+
+function ldlibs()
+    libname = if ccall(:jl_is_debugbuild, Cint, ()) != 0
+        "julia-debug"
+    else
+        "julia"
+    end
+    if Sys.isunix()
+        return "-Wl,-rpath,$(shell_escape(libDir())) " *
+            (Sys.isapple() ? string() : "-Wl,-rpath,$(shell_escape(private_libDir())) ") *
+            "-l$libname"
+    else
+        return "-l$libname -lopenlibm"
+    end
+end
+
+function cflags()
+    flags = IOBuffer()
+    print(flags, "-std=gnu99")
+    include = shell_escape(includeDir())
+    print(flags, " -I", include)
+    if Sys.isunix()
+        print(flags, " -fPIC")
+    end
+    return String(take!(flags))
+end
