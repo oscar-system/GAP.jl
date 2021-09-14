43ed5e90539959828fc831d6edd8678fcd31d0e7
Max Horn <max@quendi.de>
2021-09-14 17:17:22 +0200

    Remove LoadPackageAndExposeGlobals
    This function always was very peculiar and for special interests only.
    It is also very easily abused resp. used in a problematic way.
    
    I believe that any code which uses this right now should be rewritten
    (and I'll be happy to help with that by suggesting ways to do it).
    Alternatively, one can of course also copy the code deleted by this
    patch into one's own code.

--- test/packages.jl
+++ test/packages.jl
@@ -12,16 +12,3 @@
 #    @test GAP.Packages.remove("io", interactive = false, pkgdir = pkgdir)
 
 end
-
-@testset "LoadPackageAndExposeGlobals" begin
-
-    LoadPackageAndExposeGlobals("GAPDoc", "test1")
-    @test isdefined(test1, :SymmetricGroup) == false
-
-    LoadPackageAndExposeGlobals("GAPDoc", "test2", all_globals = true)
-    @test isdefined(test2, :SymmetricGroup) == true
-
-    LoadPackageAndExposeGlobals("GAPDoc", test3, all_globals = true)
-    @test test3.SymmetricGroup == 5
-
-end
