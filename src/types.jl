258c839584f8561ee17d9e8d1e31026235f700ac
Max Horn <max@quendi.de>
2021-09-17 16:57:55 +0200

    Add GapInt type union

--- src/types.jl
+++ src/types.jl
@@ -82,4 +82,18 @@ const GapObj = GAP_jll.MPtr
 # TODO: should we document Obj?
 const Obj = Union{GapObj,FFE,Int64,Bool,Nothing}
 
-export FFE, GapObj
+"""
+    GapInt
+
+Any GAP integer object is represened in Julia as either a `GapObj` (if it
+is a "large" integer) or as an `Int` (if it is a "small" integer). This
+type union can be used to express this conveniently, e.g. when one wants to
+help type stability.
+
+Note that also GAP's `infinity` and `-infinity` fit under this type (as do
+many other objects which are not numbers).
+"""
+const GapInt = Union{GapObj,Int}
+
+# Don't export GAP.Obj!
+export FFE, GapObj, GapInt
