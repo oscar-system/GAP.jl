49b5c44a74396a380c2ce890949dd4c384cb0c47
ThomasBreuer <sam@math.rwth-aachen.de>
2023-01-27 13:52:26 +0100

    catch `Downloads.download` errors
    addresses issue #852

--- src/packages.jl
+++ src/packages.jl
@@ -19,9 +19,12 @@ function init_packagemanager()
     # overwrite PKGMAN_DownloadURL
     Globals.MakeReadWriteGlobal(GapObj("PKGMAN_DownloadURL"))
     Globals.PKGMAN_DownloadURL = function(url)
-        # exception handling is omitted by concept, i.e. errors occurring during the download are shown to the user
+      try
         buffer = Downloads.download(String(url), IOBuffer())
         return GapObj(Dict{Symbol, Any}(:success => true, :result => String(take!(buffer))), recursive=true)
+      catch
+        return GapObj(Dict{Symbol, Any}(:success => false), recursive=true)
+      end
     end
     Globals.MakeReadOnlyGlobal(GapObj("PKGMAN_DownloadURL"))
 
