e80e0f612749b134d21d7563e12822b4a5e174c2
ThomasBreuer <sam@math.rwth-aachen.de>
2023-06-13 10:41:38 +0200

    added some tests
    (but these are still not enough)

--- test/packages.jl
+++ test/packages.jl
@@ -11,4 +11,21 @@
 #    @test GAP.Packages.install("fga", interactive = false, pkgdir = pkgdir)
 #    @test GAP.Packages.remove("fga", interactive = false, pkgdir = pkgdir)
 
+    # Load packages via their local paths.
+    # - a package that was already loaded, with the same path
+    path = string(GAP.Globals.GAPInfo.PackagesLoaded.ctbllib[1])
+    @test GAP.Packages.load(path)
+
+    # - a package that was already loaded, with another installation path
+#TODO: How to guarantee two installed versions with different paths?
+
+    # - a package that was not yet loaded (only once in a Julia session)
+    if ! GAP.Globals.IsPackageLoaded(GAP.GapObj("autodoc"))
+      path = string(GAP.Globals.GAPInfo.PackagesInfo.autodoc[1].InstallationPath)
+      @test GAP.Packages.load(path)
+    end
+
+    # - a nonexisting path
+    path = path * "xxx"
+    @test ! GAP.Packages.load(path)
 end
