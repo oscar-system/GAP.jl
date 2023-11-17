48d13e419eb1210440a96773471ffdbc4132d884
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-11-17 09:28:59 +0100

    Bump Aqua to 0.8 and use more defaults (#955)
    * Bump Aqua to 0.8 and use more defaults
    
    * Remove unneeded dependency `GMP_jll`
    
    * Add missing compat entry
    
    * Bump again

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -3,13 +3,6 @@ using Aqua
 @testset "Aqua.jl" begin
     Aqua.test_all(
         GAP;
-        ambiguities=true,
-        unbound_args=true,
-        undefined_exports=true,
-        project_extras=true,
-        stale_deps=true,
-        deps_compat=true,
-        project_toml_formatting=true,
-        piracy=(GAP.use_jl_reinit_foreign_type() ? true : (treat_as_own=[GapObj],))
+        piracies=(GAP.use_jl_reinit_foreign_type() ? true : (treat_as_own=[GapObj],))
     )
 end
