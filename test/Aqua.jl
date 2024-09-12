d53926d22b218be022c68bcdf4677502686951b5
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-09-12 15:04:07 +0200

    Remove banner code and instead use `should_show_banner` (#1027)

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -3,6 +3,7 @@ using Aqua
 @testset "Aqua.jl" begin
     Aqua.test_all(
         GAP;
+        ambiguities=false, # some from AbstractAlgebra.jl show up here
         piracies=(GAP.use_jl_reinit_foreign_type() ? true : (treat_as_own=[GapObj],))
     )
 end
