8aefe07c6d868fea54ff78cea80da096c87ed71e
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-11-21 20:01:56 +0100

    Add `GAP.Packages.test`
    Co-authored-by: Max Horn <max@quendi.de>

--- src/packages.jl
+++ src/packages.jl
@@ -3,7 +3,8 @@ module Packages
 
 import Downloads
 import Pidfile
-import ...GAP: Globals, GapObj, replace_global!, RNamObj, sysinfo, Wrappers
+import Test: @test
+import ...GAP: disable_error_handler, Globals, GapObj, replace_global!, RNamObj, sysinfo, Wrappers
 
 const DEFAULT_PKGDIR = Ref{String}()
 const DOWNLOAD_HELPER = Ref{Downloads.Downloader}()
@@ -406,6 +407,67 @@ function build(name::String; quiet::Bool = false,
     Wrappers.SetInfoLevel(Globals.InfoPackageManager, oldlevel)
   end
   return res
+end
+
+"""
+    test(name::String)
+
+Test the GAP package with name `name`.
+
+The function uses [the function `TestPackage`](GAP_ref(ref:TestPackage)).
+
+Use-sites of this function should verify that the `Test` package is loaded
+before calling this function, as this function might be moved
+to a package extension that depends on the `Test` package in the future.
+"""
+function test(name::String)
+  global disable_error_handler
+  
+  function with_gap_var(f, name::String, val)
+    gname = GapObj(name)
+    old_value = Globals.ValueGlobal(gname)
+    Globals.MakeReadWriteGlobal(gname)
+    Globals.UnbindGlobal(gname)
+    Globals.BindGlobal(gname, val)
+    try
+        f()
+    finally
+      Globals.MakeReadWriteGlobal(gname);
+      Globals.UnbindGlobal(gname);
+      Globals.BindGlobal(gname, old_value);
+    end
+  end
+
+  error_occurred = false
+  ended_using_QuitGap = false
+  function fake_QuitGap(code)
+    if code != 0
+      error_occurred = true
+    end
+    ended_using_QuitGap = true
+  end
+
+  disable_error_handler[] = true
+  try
+    with_gap_var("ERROR_OUTPUT", Globals._JULIAINTERFACE_ORIGINAL_ERROR_OUTPUT) do
+      with_gap_var("QuitGap", fake_QuitGap) do
+        with_gap_var("QUIT_GAP", fake_QuitGap) do
+          with_gap_var("ForceQuitGap", identity) do
+            with_gap_var("FORCE_QUIT_GAP", identity) do
+              result = Globals.TestPackage(GapObj(name))
+              @test ended_using_QuitGap || result == true
+              # Due to the hack above, we run into an error in TestPackage that is usually unreachable.
+              # In the case of a `QuitGap` call, we thus don't check for `result == true`.
+            end
+          end
+        end
+      end
+    end
+  finally
+    disable_error_handler[] = false
+  end
+
+  @test !error_occurred
 end
 
 """
