e5ef53e5a7c763b098bd9f8b09f5cfddc2ceae8b
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-01-24 15:10:46 +0100

    Reduce number of doctest runs

--- docs/make.jl
+++ docs/make.jl
@@ -137,7 +137,7 @@ DocMeta.setdocmeta!(GAP, :DocTestSetup, :(using GAP, GAP.Random); recursive = tr
 makedocs(
     sitename = "GAP.jl",
     modules = [GAP],
-    doctest = true,
+    doctest = false,
     doctestfilters = GAP.GAP_doctestfilters,
     pages = GAP.GAP_docs_pages,
 )
