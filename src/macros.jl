1916972b7189a24bb7570eaa12d42f1b0148a514
Max Horn <max@quendi.de>
2025-02-07 00:46:01 +0100

    Apply suggestions from code review

--- src/macros.jl
+++ src/macros.jl
@@ -416,7 +416,7 @@ macro wrap(ex)
 
     return esc(MacroTools.@qq begin
        @eval const $newsym = Ref{GapObj}()
-       global $name;
+       global $name
        Base.@__doc__ $(Expr(:call, name, lhsargs...)) = $body
     end)
 end
