20c4171e49f643b347ff91dc45746fb6c4215eeb
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-02-27 18:09:07 +0100

    Improve `expect` script

--- etc/expect.sh
+++ etc/expect.sh
@@ -9,4 +9,4 @@ export GAP_PRINT_BANNER=false
 # force Julia to use full REPL
 export TERM=xterm
 
-expect -c "spawn julia --startup-file=no --history-file=no --banner=no $*" etc/julia.expect
+expect -c "spawn julia --startup-file=no --history-file=no --banner=no $* -e \"atreplinit() do repl; if VERSION >= v\\\"1.11.0-0\\\"; repl.options.hint_tab_completes = false; end; end;\" -i" etc/julia.expect
