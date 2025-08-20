2e32a881805aa78c5a0fbecb74555e5744f97052
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-08-20 11:42:32 +0200

    Fix another JET-reported issue

--- src/GAP.jl
+++ src/GAP.jl
@@ -230,7 +230,7 @@ function __init__()
     cmdline_options = ["", "-l", join(roots, ";")]
 
     # tell GAP about all artifacts that contain GAP packages
-    pkg_artifacts = filter(startswith("GAP_pkg_"), keys(TOML.parsefile(find_artifacts_toml(@__FILE__))))
+    pkg_artifacts = filter(startswith("GAP_pkg_"), keys(TOML.parsefile(find_artifacts_toml(@__FILE__)::String)))
     pkgdirs = [realpath(@artifact_str(name)) for name in pkg_artifacts]
     push!(pkgdirs, abspath(@__DIR__, "..", "pkg", "JuliaInterface"))
     push!(pkgdirs, abspath(@__DIR__, "..", "pkg", "JuliaExperimental"))
