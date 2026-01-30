93bd0976f6915026d0ee3f2a596e5158ccab74cd
Max Horn <max@quendi.de>
2026-01-30 15:08:34 +0100

    test: import IOCapture in the file that needs it
    This makes it easier to run the test files in isolation.

--- test/basics.jl
+++ test/basics.jl
@@ -9,6 +9,8 @@
 ##  SPDX-License-Identifier: LGPL-3.0-or-later
 ##
 
+using IOCapture
+
 @testset "basics" begin
     @test GAP.CSTR_STRING(GAP.Globals.String(GAP.Globals.PROD(2^59, 2^59))) ==
           "332306998946228968225951765070086144"
