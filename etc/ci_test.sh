a14e89a622bb9a0e8cff476e3f1898800c078f37
Max Horn <max@quendi.de>
2026-01-31 21:01:59 +0100

    tweak

--- etc/ci_test.sh
+++ etc/ci_test.sh
@@ -8,10 +8,10 @@ AnyFailures=No
 # Force recompilation of JuliaInterface with coverage instrumentation
 export CFLAGS="--coverage"
 export LDFLAGS="--coverage"
-export FORCE_JULIAINTERFACE_COMPILATION=true
-
 mkdir -p coverage
+
 #
+export FORCE_JULIAINTERFACE_COMPILATION=true
 cd pkg/JuliaInterface
 pwd
 ${GAP} --cover ../../coverage/JuliaInterface.coverage -r makedoc.g tst/testall.g || AnyFailures=Yes
@@ -19,6 +19,7 @@ gcov -o gen/src/ src/*.c*
 cd ../..
 
 #
+export FORCE_JULIAINTERFACE_COMPILATION=
 cd pkg/JuliaExperimental
 pwd
 ${GAP} --cover ../../coverage/JuliaExperimental.coverage -r makedoc.g tst/testall.g || AnyFailures=Yes
