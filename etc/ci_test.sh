1f2933b9c65414ee398be6f0764f581067aa3548
Max Horn <max@quendi.de>
2026-01-31 20:43:09 +0100

    CI: change use of FORCE_JULIAINTERFACE_COMPILATION
    So far we set FORCE_JULIAINTERFACE_COMPILATION in one invocation
    of gap.sh; but then omitted it for the next couple. But then those
    other runs will use the stock version from the JLL again, defeating
    the whole point of forcing recompilation with `--coverage` enabled.

--- etc/ci_test.sh
+++ etc/ci_test.sh
@@ -5,24 +5,26 @@ set -x
 
 AnyFailures=No
 
+# Force recompilation of JuliaInterface with coverage instrumentation
+export CFLAGS="--coverage"
+export LDFLAGS="--coverage"
 mkdir -p coverage
 #
 cd pkg/JuliaInterface
 pwd
 # Force recompilation of JuliaInterface with coverage instrumentation.
 # Use a fixed build directory so that gcov can find .gcno/.gcda files.
-CFLAGS="--coverage" LDFLAGS="--coverage" FORCE_JULIAINTERFACE_COMPILATION=tmp ${GAP} --nointeract
-${GAP} makedoc.g
-${GAP} --cover ../../coverage/JuliaInterface.coverage -r tst/testall.g || AnyFailures=Yes
+export FORCE_JULIAINTERFACE_COMPILATION=tmp
+${GAP} --cover ../../coverage/JuliaInterface.coverage -r makedoc.g tst/testall.g || AnyFailures=Yes
 gcov -o tmp/gen/src/ src/*.c*
 rm -rf tmp # Delete the coverage instrumentation in JuliaInterface again
 cd ../..
 
 #
+export FORCE_JULIAINTERFACE_COMPILATION=
 cd pkg/JuliaExperimental
 pwd
-${GAP} makedoc.g
-${GAP} --cover ../../coverage/JuliaExperimental.coverage -r tst/testall.g || AnyFailures=Yes
+${GAP} --cover ../../coverage/JuliaExperimental.coverage -r makedoc.g tst/testall.g || AnyFailures=Yes
 cd ../..
 
 if [ ${AnyFailures} = Yes ]
