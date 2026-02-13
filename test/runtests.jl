e9a65d030133b5e312ae9531ece1c2a1b887bf8e
Max Horn <max@quendi.de>
2026-02-13 11:57:53 +0100

    HACK/experiment

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,8 +25,8 @@ include("packages.jl")
 include("help.jl")
 include("rand.jl")
 
-if Base.JLOptions().code_coverage == 0
-  # REPL completion doesn't work in Julia >= 1.10 when code coverage
+if Base.JLOptions().code_coverage == 0 && VERSION < v"1.11"
+  # REPL completion doesn't work in Julia 1.10 when code coverage
   # tracking is active. For more details see the discussions at
   # <https://github.com/oscar-system/GAP.jl/pull/914> and
   # <https://github.com/JuliaLang/julia/issues/49978>.
