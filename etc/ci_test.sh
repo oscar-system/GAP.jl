57f90c935b74b410f7053a422a428019fd7d5a2c
Max Horn <max@quendi.de>
2026-02-23 09:57:20 +0100

    Fix dir

--- etc/ci_test.sh
+++ etc/ci_test.sh
@@ -11,7 +11,7 @@ cd pkg/JuliaInterface
 pwd
 # Force recompilation of JuliaInterface with coverage instrumentation.
 # Use a fixed object directory so that gcov can find .gcno/.gcda files.
-CFLAGS="--coverage" LDFLAGS="--coverage" FORCE_JULIAINTERFACE_COMPILATION=gen/src ${GAP} --nointeract
+CFLAGS="--coverage" LDFLAGS="--coverage" FORCE_JULIAINTERFACE_COMPILATION=. ${GAP} --nointeract
 ${GAP} makedoc.g
 ${GAP} --cover ../../coverage/JuliaInterface.coverage -r tst/testall.g || AnyFailures=Yes
 gcov -o gen/src/ src/*.c*
