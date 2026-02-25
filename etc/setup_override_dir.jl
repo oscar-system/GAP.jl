21c4e6d038b1367241c547f86ec1783264b8689e
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-02-25 11:36:49 +0100

    Copy override setup improvements from Singular.jl

--- etc/setup_override_dir.jl
+++ etc/setup_override_dir.jl
@@ -10,28 +10,55 @@ length(ARGS) >= 1 || error("must provide path of GAP source directory as first a
 length(ARGS) >= 2 || error("must provide path of destination directory as second argument")
 gap_prefix = popfirst!(ARGS)
 prefix = popfirst!(ARGS)
-if length(ARGS) > 0 && ARGS[1] == "--debug"
-  debugmode = true
-  popfirst!(ARGS)
+
+if length(ARGS) > 0 && !startswith(ARGS[1], "--")
+  build_dir = popfirst!(ARGS)
 else
-  debugmode = false
+  build_dir = mktempdir(; cleanup = true)
 end
 
-# TODO: should the user be allowed to provide a tmp_gap_build_dir ? that might
-# be handy for incremental updates
+run_configure = true
+overwrite_allow = false
+verbose = false
+debugmode = false
+left_ARGS = String[]
+while !isempty(ARGS)
+   arg = popfirst!(ARGS)
+   if arg == "--no-configure"
+      global run_configure = false
+   elseif arg == "--yes"
+      global overwrite_allow = true
+   elseif arg == "--verbose"
+      global verbose = true
+   elseif arg == "--debug"
+      global debugmode = true
+   else
+      push!(left_ARGS, arg)
+   end
+end
+
 
 
 # validate arguments
 isdir(gap_prefix) || error("The given GAP prefix '$(gap_prefix)' is not a valid directory")
 if ispath(prefix)
-    error("installation prefix '$(prefix)' already exists, please remove it before running this script")
-    # TODO: prompt the user for whether to delete the dir or abort
+   if !overwrite_allow
+      print("The given installation prefix '$(prefix)' already exists. Overwrite? [Y/n] ")
+      overwrite_allow = lowercase(readline()) in ["y", "yes", ""]
+   end
+   if overwrite_allow
+      rm(prefix; force=true, recursive=true)
+   else
+      error("Aborting")
+   end
 end
 
 # convert into absolute paths
 mkpath(prefix)
 prefix = abspath(prefix)
 gap_prefix = abspath(gap_prefix)
+mkpath(build_dir)
+build_dir = abspath(build_dir)
 
 #
 # Install needed packages
@@ -39,73 +66,53 @@ gap_prefix = abspath(gap_prefix)
 @info "Install needed packages"
 using Pkg
 using Artifacts
-Pkg.add(["GMP_jll"])
+Pkg.add(["JLLPrefixes"])
 Pkg.instantiate()
 
-using GMP_jll
+using JLLPrefixes
 
-# In Julia >= 1.6, there is a "fake" GMP_jll which does not include header files;
-# see <https://github.com/JuliaLang/julia/pull/38797#issuecomment-741953480>
-function gmp_artifact_dir()
-    artifacts_toml = joinpath(dirname(dirname(Base.pathof(GMP_jll))), "StdlibArtifacts.toml")
+cd(build_dir)
 
-    # If this file exists, it's a stdlib JLL and we must download the artifact ourselves
-    if isfile(artifacts_toml)
-        meta = artifact_meta("GMP", artifacts_toml)
-        hash = Base.SHA1(meta["git-tree-sha1"])
-        if !artifact_exists(hash)
-            dl_info = first(meta["download"])
-            Pkg.Artifacts.download_artifact(hash, dl_info["url"], dl_info["sha256"])
-        end
-        return artifact_path(hash)
-    end
 
-    # Otherwise, we can just use the artifact directory given to us by GMP_jll
-    return GMP_jll.find_artifact_dir()
+if run_configure
+   @info "Configuring GAP in $(build_dir) for $(prefix)"
+
+   deps = ["GMP_jll"]
+   artifact_paths = collect_artifact_paths(deps)
+   deps_path = mktempdir(; cleanup=false)
+   deploy_artifact_paths(deps_path, artifact_paths) # collect all (transitive) dependencies into one tree
+
+   juliabin = joinpath(Sys.BINDIR, Base.julia_exename())
+
+   extraargs = ["CPPFLAGS=-DUSE_GAP_INSIDE_JULIA=1"]
+
+   if debugmode
+      # compile GAP in debug mode (enables many additional assertions in the kernel)
+      # and disable optimizations, so that debugging the resulting binary with gdb or lldb
+      # gets easier
+      @info "Debug mode is enabled"
+      append!(extraargs, ["CFLAGS=-g", "CXXFLAGS=-g", "--enable-debug"])
+   end
+
+   configure_cmd = `$(gap_prefix)/configure
+      --prefix=$(prefix)
+      --with-gmp=$(deps_path)
+      --with-gc=julia
+      --with-julia=$(juliabin)
+      $(extraargs)
+      $(left_ARGS)
+      `
+
+   verbose && @show configure_cmd
+
+   # TODO: redirect the output of configure into a log file
+   @show run(configure_cmd)
 end
 
-#
-# locate GMP headers and the Julia executable for use by the GAP build system
-#
-gmp_prefix = gmp_artifact_dir()
-juliabin = joinpath(Sys.BINDIR, Base.julia_exename())
-
-#
-# create a temporary directory for the build
-#
-tmp_gap_build_dir = mktempdir(; cleanup = true)
-cd(tmp_gap_build_dir)
-
-#
-# configure and build GAP
-#
-@info "Configuring GAP in $(tmp_gap_build_dir) for $(prefix)"
-
-if debugmode
-    # compile GAP in debug mode (enables many additional assertions in the kernel)
-    # and disable optimizations, so that debugging the resulting binary with gdb or lldb
-    # gets easier
-    @info "Debug mode is enabled"
-    extraargs = ["CFLAGS=-g", "CXXFLAGS=-g", "--enable-debug"]
-else
-    extraargs = []
-end
-push!(extraargs, "CPPFLAGS=-DUSE_GAP_INSIDE_JULIA=1")
-
-# TODO: redirect the output of configure into a log file
-@show run(`$(gap_prefix)/configure
-    --prefix=$(prefix)
-    --with-gmp=$(gmp_prefix)
-    --with-gc=julia
-    --with-julia=$(juliabin)
-    $(extraargs)
-    $(ARGS)
-    `)
-
-@info "Building GAP in $(tmp_gap_build_dir)"
+@info "Building GAP in $(build_dir)"
 
 # first build the version of GAP without gac generated code
-run(`make -j$(Sys.CPU_THREADS) build/gap-nocomp`)
+run(`make -j$(Sys.CPU_THREADS) build/gap-nocomp $(verbose ? "V=1" : "")`)
 
 # cheating: the following assumes that GAP in gap_prefix was already compiled...
 # we copy some generated files, so that we don't have to re-generate them,
@@ -116,17 +123,11 @@ for f in ["c_oper1.c", "c_type1.c"]
 end
 
 # complete the build
-run(`make -j$(Sys.CPU_THREADS)`)
+run(`make -j$(Sys.CPU_THREADS) $(verbose ? "V=1" : "")`)
 
-#
-# "Install" GAP
-#
+
 @info "Installing GAP to $(prefix)"
-
-# install GAP binaries, headers, libraries
 run(`make install-bin install-headers install-libgap install-sysinfo install-gaproot`)
-
-
 # We deliberately do NOT install the GAP library, documentation, etc. because
 # they are identical across all platforms; instead, we use another platform
 # independent artifact to ship them to the user.
